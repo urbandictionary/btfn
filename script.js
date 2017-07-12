b24fb978d1ff78b320406deb41fe98dff2c16adc
apeckham <apeckham@gmail.com>
2017-07-11 23:12:22 -0700

    bg url

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var css = document.createElement("style");
 css.type = "text/css";
 css.innerHTML = "\
 body {\
-  background: url('http://localhost:8080/bg.jpg') no-repeat fixed center !important;\
+  background: url('https://urbandictionary.github.io/btfn/bg.jpg') no-repeat fixed center !important;\
   background-size: cover !important;\
 }\
 #urban-top-bar, #dark_top { background-color: transparent !important }\
